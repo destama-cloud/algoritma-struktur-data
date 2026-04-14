f64d2e6f3be91df99532443e6690fd3889bd841c
destama-cloud <insinfern@gmail.com>
2026-04-14 19:16:58 +0700

    task 2

---
+++ jobsheet 2/mahasiswa.java
@@ -0,0 +1,50 @@
+public class mahasiswa {
+
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    // Default constructor
+    public mahasiswa() {
+    }
+
+    // Parameterized constructor
+    public mahasiswa(String nama, String nim, double ipk, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.ipk = ipk;
+        this.kelas = kelas;
+    }
+
+    void tampilkanInformasi() {
+        System.out.println("Nama  : " + nama);
+        System.out.println("NIM   : " + nim);
+        System.out.println("IPK   : " + ipk);
+        System.out.println("Kelas : " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("GPA is invalid. Must be between 0.0 and 4.0");
+        }
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
